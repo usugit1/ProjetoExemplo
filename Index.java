80aa2bd751fe2a713e8bbe287e1f59deef605753
Outro Usuário <usu.git2@gmail.com>
2020-03-02 17:18:32 -0300

    Inclusão de comentário

--- Index.java
+++ Index.java
@@ -4,7 +4,7 @@ public class Index {
 	public static void main(String[] args) {
         String[] alunos = { "João", "Maria", "Pedro", "Ana", "Carlos" };
         System.out.println("LISTA DE NOMES:");
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 5; i++) // Imprime a lista de alunos.
         	System.out.println(alunos[i]);
 	}
 }
