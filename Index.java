ebd8128db60bc35fc7e79002fa0152eeb0526559
Leonardo Bertholdo <usu.git1@gmail.com>
2020-03-03 17:44:48 -0300

    Alteração para gravação de dados em arquivo texto

--- Index.java
+++ Index.java
@@ -1,10 +1,20 @@
 package exemplo;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Index {
 	public static void main(String[] args) {
-        String[] alunos = { "Jo√£o", "Maria", "Pedro", "Ana", "Carlos" };
-        System.out.println("LISTA DE NOMES:");
-        for (int i = 0; i < 5; i++) // Imprime a lista de alunos.
-        	System.out.println(alunos[i]);
+        String[] alunos = { "Jo„o", "Maria", "Pedro", "Ana", "Carlos" };
+        try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter("alunos.txt"));
+			bw.write("LISTA DE NOMES:\n");
+			for (int i = 0; i < 5; i++)
+				bw.write(alunos[i] + "\n");
+			bw.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
