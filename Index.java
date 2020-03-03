ebf51cc22fdf7b6995eb0b6bf76f710be7f5ed5c
usugit1 <61211542+usugit1@users.noreply.github.com>
2020-03-03 17:53:00 -0300

    Comentário para tratamento de exceção

--- Index.java
+++ Index.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Index {
 	public static void main(String[] args) { // Método main
         String[] alunos = { "João", "Maria", "Pedro", "Ana", "Carlos" };
-        try {
+        try { // Tratamento de exceção
 			BufferedWriter bw = new BufferedWriter(new FileWriter("alunos.txt"));
 			bw.write("LISTA DE NOMES:\n");
 			for (int i = 0; i < 5; i++)
